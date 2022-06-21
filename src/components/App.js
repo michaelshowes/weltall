c501d29a457b019ef7aca5deb5264394617ba7f5
Michael Showes <michaels@interactivestrategies.com>
2022-06-21 15:44:27 -0400

    minor changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import './App.scss'
+import './App.scss';
+import Header from './Header/Header'
 
 export default function App() {
   return (
     <div className='App'>
-
+      <Header />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-require('file-loader?name=[name].[ext]!./index.html')
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './global-scss/index.scss';
