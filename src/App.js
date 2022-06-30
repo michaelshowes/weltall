f66a6756aed2cde4b6897a12fa5f5d63b53233e8
Michael Showes <michaels@interactivestrategies.com>
2022-06-30 03:01:42 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header'
 
 export default function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <Header />
     </div>
   )
